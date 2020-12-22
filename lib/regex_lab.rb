7ad50fab4ad58c879caf92329ced3b48224a9077
Taylor Treece <tayloratreece@gmail.com>
2020-12-22 17:25:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d\d\d-\d\d\d-\d\d\d\d/)
+    return true
+  else
+    return false
 end
