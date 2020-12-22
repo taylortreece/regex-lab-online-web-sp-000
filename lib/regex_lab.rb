68458e2f21bb26d5853070be3cfeabe5fb12c68d
Taylor Treece <tayloratreece@gmail.com>
2020-12-22 16:09:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b(?i)[aeiou]/)
+    return true
+  else
+    return false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/[un.*][ing]/)
 end
 
 def words_five_letters_long(text)
